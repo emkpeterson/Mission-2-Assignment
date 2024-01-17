12683f17834d833c1d27b33f021c0649120134e0
emkpeterson <155405148+emkpeterson@users.noreply.github.com>
2024-01-17 11:48:24 -0700

    mission2 folder from visual studio in parallels

---
+++ mission2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using mission2;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // initialize my rumrolls variable 
+        int numRolls = 0;
+
+        // welcome the user
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // ask the user how many dice rolls they want
+        System.Console.WriteLine("\nHow many dice rolls would you like to simulate?");
+
+        //store the number input in the numrolls variable 
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        //create an instance of dice rolls 
+        DiceRoll diceroll = new DiceRoll();
+
+        // simulate dice rolls and create results array
+        int[] results = diceroll.SimulateRolls(numRolls);
+
+        //print histogram
+        PrintHistogram.PrintHist(results);
+
+        // say goodbye
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
