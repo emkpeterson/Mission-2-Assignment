2a2a44f2559910c4fb7fe10f320f110badea6545
emkpeterson <155405148+emkpeterson@users.noreply.github.com>
2024-01-22 10:11:10 -0700

    Add files via upload

--- mission2/Program.cs
+++ mission2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using mission2;
+using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
@@ -15,7 +16,7 @@ internal class Program
         Console.WriteLine("Welcome to the dice throwing simulator!");
 
         // ask the user how many dice rolls they want
-        System.Console.WriteLine("\nHow many dice rolls would you like to simulate?");
+        System.Console.Write("\nHow many dice rolls would you like to simulate? ");
 
         //store the number input in the numrolls variable 
         numRolls = int.Parse(System.Console.ReadLine());
@@ -27,9 +28,30 @@ internal class Program
         int[] results = diceroll.SimulateRolls(numRolls);
 
         //print histogram
-        PrintHistogram.PrintHist(results);
+        PrintHistogram(results);
 
         // say goodbye
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    static void PrintHistogram(int[] results)
+    {
+
+        // calculate total rolls
+        int totalRolls = results.Sum();
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + totalRolls + ".\n");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            //calculate the percentage
+            double percentage = (double)results[i] / totalRolls * 100;
+
+            //print the actual histogram
+            int asteriskCount = (int)Math.Round(percentage / 1);
+            Console.Write($"{i}: {new string('*', asteriskCount)}\n");
+        }
     }
 }
